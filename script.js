206dc8617ebd9afa2caa5248923d2bcb2a2dce7b
CL <cl.czayas@gmail.com>
2022-10-19 19:24:11 -0400

    Added arrays

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var number=["0123456789"]
+var numbers=number.split("")
+var lower=["abcdefghijklmnopqrstuvwxyz"]
+var lowers=lower.split("")
+var upper=["ABCDEFGHIKLMNOPQRSTUVWXYZ"]
+var uppers=upper.split("")
+
+function generatePassword() {
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  passwordText.innerHTML
 }
 
 // Add event listener to generate button
