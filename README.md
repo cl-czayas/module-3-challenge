a1cbe75946b0e908f7422fab9683eaec29b15421
CL <cl.czayas@gmail.com>
2022-10-19 21:03:16 -0400

    Finished code, changed character arrays because split was not working properly, edited README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# module-3-challenge
 # 03 JavaScript: Password Generator
+## Access
+
+https://cl-czayas.github.io/module-3-challenge/
+
+## Description
+
+This website was created in order to generate a random password as per the user's specifications.
+
+## Usage
+
+After accessing the webpage, users click the 'generate password' button & anser the corresponding prompts in order to generate a random password. 
+
+
+## Credits
+
+Code refactored by Christopher L. Cristóbal Zayas (cl-czayas).
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var number=["0123456789"]
-var numbers=number.split("")
-var lower=["abcdefghijklmnopqrstuvwxyz"]
-var lowers=lower.split("")
-var upper=["ABCDEFGHIKLMNOPQRSTUVWXYZ"]
-var uppers=upper.split("")
-var special=["~!@#$%^&*-+"] 
-var specials=special.split("")
+var numbers=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowers=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppers=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specials=["#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","]","[","^","_","`","'","{","|","}","~"] 
+
+var pwLength= "";
+var wantSpecial
+var wantNumber
+var wantLower
+var wantUpper
 
 function generatePassword() {
   pwLength=(prompt("Choose password length from 8-128 characters.")); 
@@ -42,6 +44,13 @@ function generatePassword() {
     if (wantUpper){
       generatedPW=generatedPW+uppers
     }
+
+    var finalPW="";
+
+    for (var i = 0; i < pwLength; i++) {
+      finalPW = finalPW + generatedPW[Math.floor(Math.random() * generatedPW.length)];
+    }
+    return finalPW;
 }
 // Write password to the #password input
 function writePassword() {
@@ -50,9 +59,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) {
-  
-}
+generateBtn.addEventListener("click", writePassword) 
